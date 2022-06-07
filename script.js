4ab062d839e3c972e0de6c6abab09fe634d0f5ef
valerii-alper <104149980+valerii-alper@users.noreply.github.com>
2022-06-07 10:48:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+myAnswer = prompt("Сколько будет 2+2 ?");
+console.log(`2+2 = ${myAnswer}`);
+myPoint = 0;
+
+if(myAnswer == 4) {
+    console.log(myPoints = myPoint + 10);
+}  else if (myAnswer === null){
+    console.log(myPoints = myPoint + 0);
+} else {
+    console.log(myPoints = myPoint + 0);
+}
+
+
+mySun = prompt("Солнце встает на востоке? (ответ да/нет)");
+console.log(`Встает ли солнце на востоке - ${mySun}`);
+
+if (mySun == "да") {
+    console.log(myPointss = myPoints + 10)
+} else if (mySun === null) {
+    console.log(myPointss = myPoints + 0);
+} else {
+    console.log(myPointss = myPoints + 0);
+}
+
+myZero = prompt("Сколько будет 5 / 0?");
+console.log(`5 / 0 = ${myZero}`);
+
+if(myZero == 0) {
+    console.log(myPointsss = myPointss + 10);
+}  else if (myZero === null){
+    console.log(myPointsss = myPointss + 0);
+} else {
+    console.log(myPointsss = myPointss + 0);
+}
+
+mySky = prompt("Какого цвета небо?");
+console.log(`Небо ${mySky} цвета`);
+
+if(mySky == "голубого" ) {
+    console.log(myPointssss = myPointsss + 10);
+}  else if (mySky === null){
+    console.log(myPointssss = myPointsss + 0);
+} else {
+    console.log(myPointssss = myPointsss + 0);
+}
+
+
+myLife= prompt("Какой правильный ответ на главный вопрос жизни, вселенной и всего такого?");
+console.log(`Ответ - ${myLife}`);
+
+if(myLife == 42 ) {
+    console.log(myPointsssss = myPointssss + 10);
+}  else if (myLife === null){
+    console.log(myPointsssss = myPointssss + 0);
+} else {
+    console.log(myPointsssss = myPointssss + 0);
+}
+
+alert(myPointsssss);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
